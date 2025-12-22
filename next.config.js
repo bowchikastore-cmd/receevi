331ef69ac1f667c3704572be6cbd466a16c46220
bowchikastore-cmd <bowchikastore@gmail.com>
2025-12-22 17:13:39 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
+  output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
+
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "https://receevi-zwy7.onrender.com"
+      ]
+    }
+  }
 }
 
 module.exports = nextConfig

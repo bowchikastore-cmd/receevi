648cba85ee92719d091f665d338b3280e984cb86
bowchikastore-cmd <bowchikastore@gmail.com>
2025-12-23 17:43:25 +0530

    Update main.js

--- function/main.js
+++ function/main.js
@@ -1,6 +1,6 @@
 module.exports = async ({ req, res }) => {
   return res.json({
     status: "ok",
-    message: "Appwrite function running"
+    message: "Appwrite function running test"
   });
 };
